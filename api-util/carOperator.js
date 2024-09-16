0c114aeacb4fbdc57a52a18a46bd9e7959167875
bozoten <shridharapavel@gmail.com>
2024-09-16 08:43:14 -0400

    Reworked Project Structure

---
+++ api-util/carOperator.js
@@ -0,0 +1,63 @@
+const express = require('express');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+
+app.get('/', (req, res) => {
+    res.send('its working.');
+});
+
+
+async function generate(state) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(state);
+    return result.response.text();
+}
+
+app.post('/api/data', async (req, res) => {
+    const receivedData = req.body;
+    const data = JSON.stringify(receivedData);
+    const actionSequence = await generate(data);
+    console.log('Received data:', receivedData);
+    console.log('action sequence', actionSequence)
+    res.json({ message: 'Data received successfully', data: actionSequence });
+  });
+  
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`The Server is running on port ${PORT}`);
+});
